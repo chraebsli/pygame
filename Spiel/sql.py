a636efdce8eb6a86a847efb876489fe8ca7f6e58
chraebsli <nicholas.joel.krebs@gmail.com>
2021-03-22 15:11:16 +0100

    awd

--- Spiel/sql.py
+++ Spiel/sql.py
@@ -2,37 +2,38 @@ import mysql.connector
 from mysql.connector import errorcode
 
 def start():
-  # Obtain connection string information from the portal
-  config = {
-    'host':'192.168.60.146',
-    'user':'coinchaser',
-    'password':'Coinchaser2021',
-    'database':'coinchaser'
-  }
+	# Obtain connection string information from the portal
+	config = {
+	#'host':'188.154.233.211', # remote
+	'host':'192.168.60.146', # local
+	'user':'coinchaser',
+	'password':'Coinchaser2021',
+	'database':'coinchaser'
+	}
 
-  # Construct connection string
-  try:
-    conn = mysql.connector.connect(**config)
-    print("Connection established")
-  except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-      print("Something is wrong with the user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-      print("Database does not exist")
-    else:
-      print(err)
-  else:
-    cursor = conn.cursor()
+	# Construct connection string
+	try:
+		conn = mysql.connector.connect(**config)
+		print("Connection established")
+	except mysql.connector.Error as err:
+		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+			print("Something is wrong with the user name or password")
+		elif err.errno == errorcode.ER_BAD_DB_ERROR:
+			print("Database does not exist")
+		else:
+			print(err)
+	else:
+		cursor = conn.cursor()
 
-    # Insert some data into table
-    cursor.execute("INSERT INTO leaderboard (crdate, playername, points, playedTime) VALUES (%s,%s,%s,%s)", ("16.03.", "test1", 400, "1:45.65"))
+	# Insert some data into table
+	cursor.execute("INSERT INTO leaderboard (crdate, playername, points, playedTime) VALUES (%s,%s,%s,%s)", ("16.03.", "test2", 400, "1:45.65"))
 
-    # Cleanup
-    conn.commit()
-    cursor.close()
-    conn.close()
-    print("Done.")
+	# Cleanup
+	conn.commit()
+	cursor.close()
+	conn.close()
+	print("Done.")
 
-    #INSERT INTO scores (crdate, playername, points, playtime) VALUES (%s,%s,%s,%s)", ("16.03.", "test1", 400, "1:45.65")
+	#INSERT INTO scores (crdate, playername, points, playtime) VALUES (%s,%s,%s,%s)", ("16.03.", "test1", 400, "1:45.65")
 
 start()
